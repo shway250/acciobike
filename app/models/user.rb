cec2d1d98e4a4fc8a3862db11ac7c20de05d03ac
ChrisKenyon79 <ckenyon79@gmail.com>
2016-09-07 10:11:02 -0700

    Jackson's auth stuff

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  validates :email,
+  presence: true,
+  uniqueness: {case_sensitive: false}
+
+  has_secure_password
+
+  validates :password,
+  length: { in: 8..72 },
+  on: :create
+
+  def self.authenticate(params)
+    User.find_by_email(params[:email]).try(:authenticate, params[:password])
+  end
+end
